35674ba78c6fd443f941f527a1fc2efeea6eeaf9
Fede <federicojose2000@gmail.com>
2024-06-24 21:35:54 +0200

    testing lights

--- 24_06/src/js/index.js
+++ 24_06/src/js/index.js
@@ -14,27 +14,48 @@ document.body.appendChild( renderer.domElement );
 //*Tipo de objeto que se crea( cubo, esfera, cono, tubo, toriode etc... )
 const geometry = new THREE.BoxGeometry();
 //*Material que se le aplica al objeto
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const material = new THREE.MeshStandardMaterial({color: 0x00ff00});
 //*Objeto para crear geometry ocn su material
 const element = new THREE.Mesh( geometry, material );
-
-
+//*Especificar que el cubo tb genreará sombras
+element.castShadow = true;
+element.position.set(1,2,1)
 //*Insertar en escena el elemento
 scene.add(element);
 
+//*Lights
+const light = new THREE.DirectionalLight(0xffffff, 1, 10);
+light.position.set(-1, 1, 1);
+//*Especificar que la luz genera sombra
+light.castShadow = true;
+scene.add(light);
+
+
 //*Añadir malla a escena
-const grid = new THREE.GridHelper(10, 10);
-scene.add(grid);
-camera.position.z = 15;
-camera.position.y = -10;
-camera.rotation.y = -10;
-camera.rotation.x = 10;
+// const grid = new THREE.GridHelper(10, 10);
+// scene.add(grid);
+camera.position.z = 5;
+camera.position.y = -3;
+camera.rotation.x = .5;
+// camera.position.x = -5;
+// camera.rotation.z = 1;
+// camera.rotation.z = 1;
+
+
+//*PLANO
+const planeGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
+const planeMaterial = new THREE.MeshStandardMaterial({color: 0xff0000});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+//*Decir que el plano recibe/mostrarrá sombras
+plane.receiveShadow = true;
+plane.position.set(0, 0, 0);
+scene.add(plane);
 
 //*renderizar la escena
 function animate() {
+    // element.rotation.x += 0.01;
+    // element.rotation.y += 0.01;
     renderer.render( scene, camera );
     //*Girar el elemento
-    element.rotation.x += 0.01;
-    element.rotation.y += 0.01;
 }
 renderer.setAnimationLoop( animate );
